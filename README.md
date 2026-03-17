b6a7287943f697b3abbef9d54e95e38dec51d8ed
pcmagas <pcmagas@disroot.org>
2026-03-17 23:09:13 +0200

    First  Commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# AsciiArt
+
+A fully responsive, customizable HTML/CSS theme inspired by the charm of 7-bit ASCII art.
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+// Custom JS
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("My Custom Theme Loaded!");
+});
