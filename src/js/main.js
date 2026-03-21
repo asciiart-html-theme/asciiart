6d1a0cdb5432dea831040f6c3c85deecfc9cbe15
pcmagas <pcmagas@disroot.org>
2026-03-21 21:29:51 +0200

    Button toggle js

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-// Demo JS
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.getElementById("btn-demo");
-  if(btn){
-    btn.addEventListener("click", () => alert("Aciiiart JS works! 🎨"));
-  }
-  console.log("Aciiiart Theme Loaded!");
-});
