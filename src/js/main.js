95c81114f835c5098f8c6e29105f3c613543dbb0
pcmagas <pcmagas@disroot.org>
2026-03-22 23:29:11 +0200

    1. Button to hide a visible element 2. Documentation about how visibility should be managed. 3. Function to retrieve the control element.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
 function getDomElement(element) {
-
-  console.log(element instanceof HTMLElement);
   if (element instanceof HTMLElement){
     return element;
   } 
@@ -16,38 +14,63 @@ function getDomElement(element) {
 }
 
 /**
- * This function toggles an element's visibility.
- * It should be used upon an event listener. 
+ * Get the controlled element from original controller
+ * 
+ * 
+ * 
+ * @param {HTMLElement} controllerElement 
+ * @returns {HTMLElement} the element that controllerElementControlls
+ */
+function getControlElement(controllerElement){
+  let elementToToggle = controllerElement.dataset.controlElement
+
+  if(!elementToToggle){
+    throw new Error("No element to toggle was provided as dataset upon buttol element")
+  }
+
+  elementToToggle = getDomElement(elementToToggle)
+
+  if(!elementToToggle){
+    throw new Error("No element to toggle was found")
+  }
+
+  return elementToToggle
+}
+
+/**
+ * Upon Button clikc toggle visibility
+ * It should be used upon an event listener.
+ * 
+ * Whether the element would be hidden or not is dewtermined via data attributes.
+ * Please use css to hide the element itself.
+ * 
  * @param {HTMLElement|string} button 
  */
-function toggleSidebar(button) {
+function toggleVisibilityOnClick(button) {
+  if(!button){
+    throw new Error("Button Not defined")
+  }
 
-    if(!button){
-        throw new Error("Button Not defined")
-    }
-
-    button = getDomElement(button)
-    let elementToToggle = button.dataset.controlElement
-
-    if(!elementToToggle){
-        throw new Error("No element to toggle was provided as dataset upon buttol element")
-    }
-
-    elementToToggle = getDomElement(elementToToggle)
-
-    if(!elementToToggle){
-        throw new Error("No element to toggle was found")
-    }
+  button = getDomElement(button)
+  let elementToToggle = getControlElement(button)
     
-    console.log(elementToToggle.checkVisibility());
+  let visible = elementToToggle.checkVisibility()
+  elementToToggle.dataset.visible=!visible;
+  // The check visibiltiy is called again because we need to recheck whether css has updated the visibility
+  button.dataset.elementHidden=!elementToToggle.checkVisibility()
+}
 
-    if(elementToToggle.checkVisibility()){
-       elementToToggle.style.display="none";
-       button.dataset.elementHidden=true;
-       return
-    }
-
-    console.log("Here");
-    button.dataset.elementHidden=false;
-    elementToToggle.style.display="block"
+/**
+ * Upon Button click hide an element.
+ * The element which needs to be hidden is determined via data attributes.
+ * 
+ * How an element it woulv be hodden is determined via data attributes.
+ * 
+ * @param {HTMLElement|string} button 
+ */
+function hideVisibilityOnClick(button){
+  button = getDomElement(button)
+  let elementToToggle = getControlElement(elementToToggle)
+  elementToToggle.dataset.visible=false;
+  button.dataset.elementHidden=true
 }
