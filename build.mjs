632e2a0ab3cc636f310b5d340acf05e81f82b6c0
pcmagas <pcmagas@disroot.org>
2026-03-26 20:40:02 +0200

    test build.js

--- build.mjs
+++ build.mjs
@@ -2,32 +2,51 @@ import * as esbuild from 'esbuild'
 import { readdir } from 'fs/promises'
 import path from 'path'
 
-// Bundle main asciiart.js into a single file for traditional script usage
-await esbuild.build({
-  entryPoints: ['./src/js/asciiart.js'],
-  outfile: './dist/js/asciiart.min.js',
-  bundle: true,       // resolves imports
-  minify: true,       // minify output
-  format: 'iife',     // works in traditional <script>
-  globalName: 'AsciiArt' // exposes your exports globally
-}).catch(() => process.exit(1));
+async function buildFile(entry, outfile, format, globalName) {
+  return esbuild.build({
+    entryPoints: [entry],
+    outfile,
+    bundle: true,
+    minify: true,
+    format,
+    ...(globalName ? { globalName } : {})
+  })
+}
 
+// Main library builds
+await Promise.all([
+  buildFile('./src/js/asciiart.js', './dist/js/asciiart.min.js', 'iife'),
+  buildFile('./src/js/asciiart.js', './dist/js/asciiart.esm.min.js', 'esm')
+]).catch(() => process.exit(1))
+
+
+// Components
 const componentsDir = './src/js/components'
 const files = await readdir(componentsDir)
 
 await Promise.all(
   files
     .filter(file => file.endsWith('.js'))
-    .map(file => {
+    .flatMap(file => {
       const name = path.basename(file, '.js')
+      const entry = `${componentsDir}/${file}`
+      const globalName = name.charAt(0).toUpperCase() + name.slice(1)
 
-      return esbuild.build({
-        entryPoints: [`${componentsDir}/${file}`],
-        outfile: `./dist/js/components/${name}.min.js`,
-        bundle: true,
-        minify: true,
-        format: 'iife',
-        globalName: name.charAt(0).toUpperCase() + name.slice(1)
-      })
+      return [
+        // IIFE build
+        buildFile(
+          entry,
+          `./dist/js/components/${name}.min.js`,
+          'iife',
+          globalName
+        ),
+
+        // ESM build
+        buildFile(
+          entry,
+          `./dist/js/components/${name}.esm.min.js`,
+          'esm'
+        )
+      ]
     })
 ).catch(() => process.exit(1))
