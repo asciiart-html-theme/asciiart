aa40bb3e9a314207833962c5b81c6a29e048d266
pcmagas <pcmagas@disroot.org>
2026-03-26 20:06:01 +0200

    Export.

--- src/js/asciiart.js
+++ src/js/asciiart.js
@@ -7,3 +7,9 @@ export {
   hideVisibilityOnClick, 
   Treemenu 
 };
+
+if (typeof window !== "undefined") {
+  window.toggleVisibilityOnClick = toggleVisibilityOnClick;
+  window.hideVisibilityOnClick = hideVisibilityOnClick;
+  window.Treemenu = Treemenu;
+}
